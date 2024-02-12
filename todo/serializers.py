2447a72b8b2027cde0d55ab25718aed394f410e5
DinefGH <fabianduerr@web.de>
2024-02-12 04:47:15 +0100

    test Todo

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,7 +2,17 @@ from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 from .models import Todo
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'username', 'email']
+
+
+
+
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'created_at']
+        fields = ['id', 'title', 'description', 'created_at', 'user']
--- up.bat
+++ up.bat
@@ -1,4 +1,3 @@
 git add .
 git commit -m "%*"
 git push
-chrome "https://www.pythonanywhere.com/user/Dinef/consoles/32354911/"
