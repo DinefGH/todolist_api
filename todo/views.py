c2e62f9cddb6c88b3be3a652e7a0370e2b0cdaaf
DinefGH <fabianduerr@web.de>
2024-02-12 20:02:06 +0100

    post Todo

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
+from django.core import serializers
 from .serializers import TodoSerializer
 from .models import Todo
+from django.http import HttpResponse
 
 class TodoViewSet(viewsets.ModelViewSet):
     """
@@ -11,3 +13,10 @@ class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all().order_by('-created_at')
     serializer_class = TodoSerializer
     permission_classes = [] # permissions.IsAuthenticated
+
+    def create(self, request):
+        todo = Todo.objects.create(title= request.POST.get('title', '') ,
+                                    description= request.POST.get('description', '') ,
+                                    user= request.user)
+        serialized_obj = serializers.serialize('json', [todo, ])
+        return HttpResponse(serialized_obj, content_type='application/json')
